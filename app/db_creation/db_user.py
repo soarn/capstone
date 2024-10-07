06d34c8bf6010699dda7f9fa91352262116b014d
Jack <soarn@soarn.pro>
2024-10-07 15:01:52 -0700

    feat: begin to implement the creation of the user database

--- app/db_creation/db_user.py
+++ app/db_creation/db_user.py
@@ -1,11 +1,15 @@
-from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("CONNECTION_STRING")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("CONNECTION_STRING")
+db.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# TODO: #4 Create a User model
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
