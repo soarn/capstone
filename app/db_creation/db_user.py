303f4fba919d2ade558c46fa81dcd1d711fe20a1
Jack <soarn@soarn.pro>
2024-10-07 13:35:23 -0700

    feat: create db_user.py file for the user database creation process

---
+++ app/db_creation/db_user.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("CONNECTION_STRING")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
