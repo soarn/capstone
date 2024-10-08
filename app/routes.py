9eae11a2b51f3cbd5bb9599c33c8a952b468e13e
Erik <ebare@asu.edu>
2024-10-08 09:46:25 -0700

    feat: Added Routes and Updated Home / Portfolio Page

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,17 @@
 # Place flask routes here
 
 # TODO: #1 Implement routes
+
+#Route to Home Page
+from flask import Flask, render_template, request, jsonify
+import routes
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
