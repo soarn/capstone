a0fc6a6d6c46f67fd1014cb1e9d073526a41d9bc
Majorfeeder <144262398+majorfeeder@users.noreply.github.com>
2024-10-08 10:10:29 -0700

    Added classes for user.py and stock.py

--- app/db_creation/db_stock.py
+++ app/db_creation/db_stock.py
@@ -11,4 +11,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # TODO: #6 Create a Stock model
-# class Stock(db.Model):
+class Stock(db.Model):
+    stock_id = db.Column(db.Integer, primary_key=True)
+    stock_symbol = db.Column(db.String(10), unique=True, nullable=False)
+    company_name = db.Column(db.String(100), nullable=False)
+    current_price = db.Column(db.Float, nullable=False)
+    available_price = db.Column(db.Float, nullable=False)
--- app/db_creation/db_user.py
+++ app/db_creation/db_user.py
@@ -11,7 +11,47 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # TODO: #4 Create a User model
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    account_balance = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+
+# TODO: #5 Create account_settings
+class AccountSettings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    notification_preference = db.Column(db.Boolean, nullable=False)
+    theme_preference = db.Column(db.Boolean, nullable=False)
+
+# TODO: #6 Create Portfolio
+class Portfolio(db.Model):
+    portolio_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    stock_id = db.Column(db.Integer, db.ForeignKey('stock.stock_id'), nullable=False)
+    quantity_owned = db.Column(db.Integer, nullable=False)
+    avg_purchase_price = db.Column(db.Float, nullable=False)
+
+# TODO: #7 Create Stock
+class Stock(db.Model):
+    stock_id = db.Column(db.Integer, primary_key=True)
+    stock_symbol = db.Column(db.String(10), unique=True, nullable=False)
+    company_name = db.Column(db.String(100), nullable=False)
+    current_price = db.Column(db.Float, nullable=False)
+    available_price = db.Column(db.Float, nullable=False)
+
+# TODO: #8 Create Transaction
+
+class Transaction(db.Model):
+    transaction_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    stock_id = db.Column(db.Integer, db.ForeignKey('stock.stock_id'), nullable=False)
+    transaction_type = db.Column(db.String(10), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price_at_transaction = db.Column(db.Float, nullable=False)
+    total_amount = db.Column(db.Float, nullable=False)
+    transaction_timestamp = db.Column(db.DateTime, nullable=False)
+    
