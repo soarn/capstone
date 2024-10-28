c063f1ec1066dbd8d69f6b9f413684762afa10c2
Jack <soarn@soarn.pro>
2024-10-28 00:44:58 -0700

    feat: add mobile detection, and update templates for improved responsiveness when rendering nav elements

--- app/routes/globals.py
+++ app/routes/globals.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from db.db_models import Portfolio, Stock, User, StockHistory
 from db.db import db
 from utils import fetch_bootswatch_themes, get_gravatar_url
+import user_agents
 
 globals = Blueprint('globals', __name__)
 
@@ -24,7 +25,13 @@ def inject_stock_data():
             {"symbol": entry.Stock.symbol, "price": entry.Stock.price, "quantity": entry.Portfolio.quantity}
             for entry in user_portfolio
         ]
-        return {"stock_data": portfolio_stocks}
+        
+        if not portfolio_stocks:
+            portfolio_stocks = Stock.query.all()
+
+        all_stocks = Stock.query.all()
+        stock_list = [{"symbol": stock.symbol, "price": stock.price} for stock in all_stocks]
+        return {"stock_data": portfolio_stocks, "all_stocks": stock_list}
     else:
         # Fetch all stocks if not logged in
         all_stocks = Stock.query.all()
@@ -35,3 +42,11 @@ def inject_stock_data():
 @globals.app_context_processor
 def inject_themes():
     return {"themes": fetch_bootswatch_themes()}
+
+# USER AGENT
+@globals.app_context_processor
+def inject_device_type():
+    user_agent = request.headers.get("User-Agent")
+    ua = user_agents.parse(user_agent)
+    is_mobile = ua.is_mobile
+    return {"is_mobile": is_mobile}
